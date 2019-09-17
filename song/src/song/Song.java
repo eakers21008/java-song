3d4db1b51fc788adea747bd12bdb8e0dc5591243
21008 <21008@MININT-4FIKQAM.columbushs.com>
2019-09-17 10:21:51 -0400

    Part 1

--- song/src/song/Song.java
+++ song/src/song/Song.java
@@ -15,9 +15,57 @@ public class Song {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Code inside the main method
+        
+        System.out.println("Hard to see, ooh\n" +
+"The hang ups we have today\n" +
+"Said we need to strive for more liberty");
+        songChorus();
+        songChorus();
+        System.out.println("Hard to see, ooh\n" +
+"The hang ups we have today\n" +
+"Said we need to strive for more liberty");
+        songChorus();
+        songChorus();
+        System.out.println("Lift your- lift your-, lift yourself\n" +
+"Lift-lift your-, lift your-, lift your-, lift your-, lift-lift");
+        System.out.println("Hard to see, ooh\n" +
+"The hang ups we have today-ay-ay\n" +
+"But they don't really realize, though\n" +
+"Said we need to strive for more liberty\n" +
+"This next verse, this next verse though\n" +
+"Lift yourself up on your feet\n" +
+"These bars\n" +
+"Lift yourself up on your feet\n" +
+"Watch this, go");
+        System.out.println("Poopy-di scoop\n" +
+"Scoop-diddy-whoop\n" +
+"Whoop-di-scoop-di-poop\n" +
+"Poop-di-scoopty\n" +
+"Scoopty-whoop\n" +
+"Whoopity-scoop, whoop-poop\n" +
+"Poop-diddy, whoop-scoop\n" +
+"Poop, poop\n" +
+"Scoop-diddy-whoop\n" +
+"Whoop-diddy-scoop\n" +
+"Whoop-diddy-scoop, poop");
+                
+        
         
             
     }
-    
+    public static void songChorus(){
+        System.out.println("Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on");
+                System.out.println("Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on");
+                        System.out.println("Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on\n" +
+"Lift yourself up on your feet, let's get it on");
+        
+    }
 }
